945a007ea453cc49bd0cc5a4a477b979b84083da
Mohsen Rehman <rehmanmohsen31@gmail.com>
2026-05-21 10:57:38 +0500

    changes to allow to upload the large file

--- src/app.js
+++ src/app.js
@@ -53,8 +53,8 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
 }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
